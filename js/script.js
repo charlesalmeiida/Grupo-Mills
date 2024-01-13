0187f194428de72b4d0ac83676c1ab6b0001d10f
charlesalmeiida <charlesalmeida844@gmail.com>
2024-01-12 22:38:07 -0300

    adding slide section

---
+++ js/script.js
@@ -0,0 +1,12 @@
+var swiper = new Swiper(".cards-videos", {
+  slidesPerView: 2.5,
+  spaceBetween: 54,
+  pagination: {
+    el: ".s-depoimentos .top .swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".btn-next",
+    prevEl: ".btn-prev",
+  },
+})
